9362487c6dac369d5114b5e9d4904121d69b32cd
milanfin <milan.makipaa@gmail.com>
2023-12-27 11:18:55 +0200

    caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+
+const getData = async () => {
+  let response = await fetch("http://worldtimeapi.org/api/timezone/Europe/Helsinki", {
+                          next: { revalidate: 10 },
+                      })
+
+  let data = await response.json();
+  console.log(data)
+  return data.datetime
+
+}
+export const dynamic = 'force-dynamic'
+// 'auto' | 'force-dynamic' | 'error' | 'force-static'
+
+export default async function Page() {
+
+  let test = getData();
+
+  return (
+  <div>
+  <h1>res</h1>
+  <span>{test}</span>
+  </div>
+  )
 }
